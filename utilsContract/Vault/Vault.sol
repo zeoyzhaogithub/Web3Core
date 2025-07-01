05bca2b89adab2504b4d68bde719a0343868701b
zeoyzhaogithub <1401518843@qq.com>
2025-07-01 21:57:24 +0800

    [+]金库合约

---
+++ utilsContract/Vault/Vault.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+import "./IERC20.sol";
+
+contract Vault {
+    IERC20 public immutable token;
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    function _mint(address _to, uint256 _shares) private {
+        totalSupply += _shares;
+        balanceOf[_to] += _shares;
+    }
+
+    function _burn(address _from, uint256 _shares) private {
+        totalSupply -= _shares;
+        balanceOf[_from] -= _shares;
+    }
+
+    function deposit(uint256 _amount) external {
+        /*
+        a = amount
+        B = balance of token before deposit
+        T = total supply
+        s = shares to mint
+
+        (T + s) / T = (a + B) / B 
+
+        s = aT / B
+        */
+        uint256 shares;
+        if (totalSupply == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount * totalSupply) / token.balanceOf(address(this));
+        }
+
+        _mint(msg.sender, shares);
+        token.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function withdraw(uint256 _shares) external {
+        /*
+        a = amount
+        B = balance of token before withdraw
+        T = total supply
+        s = shares to burn
+
+        (T - s) / T = (B - a) / B 
+
+        a = sB / T
+        */
+        uint256 amount = (_shares * token.balanceOf(address(this))) /
+            totalSupply;
+        _burn(msg.sender, _shares);
+        token.transfer(msg.sender, amount);
+    }
+}
