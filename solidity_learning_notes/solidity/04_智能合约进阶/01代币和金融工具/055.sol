5140ad25d5154b9eab1db7c05d13ccd0d7f8db0b
zeoyzhaogithub <1401518843@qq.com>
2025-11-09 13:33:15 +0800

    [+]英式拍卖

---
+++ solidity_learning_notes/solidity/04_智能合约进阶/01代币和金融工具/055.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+interface IERC721 {
+    function transferFrom(address _from, address _to, uint _nftId) external;
+}
+contract EnglishAuction {
+    event Start();
+    event Bid(address indexed sender, uint amount);
+    event Withdraw(address indexed bidder, uint amount);
+    event End(address highestBidder, uint highestBid);
+    // NFT 相关信息
+    IERC721 public immutable nft;
+    uint public immutable nftId;
+    // 拍卖信息
+    address payable public immutable seller;
+    uint32 public endAt;
+    bool public started;
+    bool public ended;
+    address public highestBidder;
+    uint public highestBid;
+    mapping(address => uint) public bids;
+    // 初始化
+    constructor(address _nft, uint _nftId, uint _startingBid) {
+        nft = IERC721(_nft);
+        nftId = _nftId;
+        seller = payable(msg.sender);
+        highestBid = _startingBid;
+    }
+    // 卖家发起拍卖
+    function start() external {
+        require(msg.sender == seller, "not seller");
+        require(!started, "started");
+        started = true;
+        endAt = uint32(block.timestamp + 60);
+        nft.transferFrom(seller, address(this), nftId);
+        emit Start();
+    }
+    // 买家竞价
+    function bid() external payable {
+        require(started, "not started");
+        require(block.timestamp < endAt, "ended");
+        require(msg.value > highestBid, "value < highest bid");
+        if (highestBidder != address(0)) {
+            bids[highestBidder] += highestBid;
+        }
+        highestBid = msg.value;
+        highestBidder = msg.sender;
+        emit Bid(msg.sender, msg.value);
+    }
+    // 买家提款
+    function withdraw() external {
+        uint bal = bids[msg.sender];
+        bids[msg.sender] = 0;
+        payable(msg.sender).transfer(bal);
+        emit Withdraw(msg.sender, bal);
+    }
+    // 结束拍卖
+    function end() external {
+        require(started, "not started");
+        require(!ended, "ended");
+        require(block.timestamp >= endAt, "not ended");
+        ended = true;
+        if (highestBidder != address(0)) {
+            nft.transferFrom(address(this), highestBidder, nftId);
+            seller.transfer(highestBid);
+        } else {
+            nft.transferFrom(address(this), seller, nftId);
+        }
+        emit End(highestBidder, highestBid);
+    }
+}
